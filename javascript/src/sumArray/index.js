9cedec8b1e7e0c0698805fd88a2d6eb10d225fe8
Abner Lima <abnerlima.1997@gmail.com>
2024-01-30 19:43:24 -0300

    fix(sumArray): refactoring

--- javascript/src/sumArray/index.js
+++ javascript/src/sumArray/index.js
@@ -2,14 +2,17 @@ export function sumArray(array) {
     if (!array || array?.length <= 1) {
         return 0
     }
+    const initialValues = {
+        sum: 0,
+        max: -Infinity,
+        min: Infinity
+    }
 
-    const { sum, max, min } = array.reduce((acc, curr) => {
-        return {
-            max: curr > acc.max ? curr : acc.max,
-            min: curr < acc.min ? curr : acc.min,
-            sum: acc.sum + curr
-        }
-    }, { max: -Infinity, min: Infinity, sum: 0 })
+    const { sum, max, min } = array.reduce((acc, curr) => ({
+        max: curr > acc.max ? curr : acc.max,
+        min: curr < acc.min ? curr : acc.min,
+        sum: acc.sum + curr
+    }), initialValues)
 
     return sum - max - min
 }
